3959b203f6525e59ee4a3b08c2034ea3ecc1b959
Sagar Bhattacharya <sagar13912@gmail.com>
2017-07-07 22:43:39 +0530

    react boilerplate for projects

---
+++ app/app.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var objOne = {
+	name :'Sagar',
+	gender : 'Male'
+}
+var objTwo = {
+	age:25,
+	...objOne
+}
+console.log(objTwo);
+ReactDOM.render(
+	<h1>Boilerplate app!</h1>,
+	document.getElementById('app')
+	)
---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+let express = require('express');
+let app = express();
+app.use(express.static('public'));
+app.listen(3000,function(){
+	console.log('app started');
+})
